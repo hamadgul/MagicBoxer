a1e8c95661c8008de98c7e381ecb397175f8b641
Hamad Gul <hamadgul1@gmail.com>
2024-04-26 16:38:21 -0400

    styles.js

--- screens/FormPage.jsx
+++ screens/FormPage.jsx
@@ -1,19 +1,11 @@
-import React, { useState, Component } from "react";
-import {
-  Alert,
-  Button,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  Modal,
-} from "react-native";
+import React, { Component } from "react";
+import { Alert, Button, Text, TextInput, View, Modal } from "react-native";
 import { Form } from "native-base";
 import { Keyboard } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { generateUUID } from "three/src/math/MathUtils";
 import { pack, createDisplay } from "../packing_algo/packing";
-
+import styles from "../components/Styles";
 var Buffer = require("@craftzdog/react-native-buffer").Buffer;
 
 export default class FormPage extends React.Component {
@@ -356,85 +348,3 @@ export default class FormPage extends React.Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-  },
-  label: {
-    fontSize: 16,
-    marginTop: 20,
-    fontWeight: "bold",
-    color: "#1C6EA4",
-  },
-  input: {
-    height: 40,
-    borderColor: "#ddd",
-    borderWidth: 1,
-    marginTop: 10,
-    padding: 10,
-    fontSize: 14,
-    borderRadius: 4,
-  },
-  buttonContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  submitButton: {
-    flex: 1,
-    marginRight: 5,
-  },
-  visualizeButton: {
-    flex: 1,
-    marginLeft: 5,
-  },
-  itemBorder: {
-    borderWidth: "2px",
-    borderStyle: "solid",
-    borderColor: "#1C6EA4",
-  },
-  centeredView: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 22,
-  },
-  modalContent: {
-    margin: 20,
-    backgroundColor: "#eeeeee",
-    borderRadius: 20,
-    padding: 35,
-    alignItems: "center",
-    justifyContent: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  button: {
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-  },
-  buttonOpen: {
-    backgroundColor: "#d0e0e3",
-  },
-  buttonClose: {
-    backgroundColor: "#2196F3",
-  },
-  textStyle: {
-    color: "white",
-    fontWeight: "bold",
-  },
-  modalButtonContainer: {
-    justifyContent: "space-evenly",
-    flexDirection: "row",
-    alignContent: "flex-start",
-    flexWrap: "wrap",
-  },
-});
